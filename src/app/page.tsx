c85822bfa5c3818d5edf0fd8aabf7d056c3f8782
Nelson Cadenas <nca1478@yahoo.com>
2025-07-23 23:11:36 -0400

    189. NextJS Fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { titleFont } from "@/config/fonts";
+
 export default function Home() {
     return (
         <main>
             <h1>Hola Mundo</h1>
+            <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
+            <h1 className={`${titleFont.className}`}>Hola Mundo</h1>
         </main>
     );
 }
