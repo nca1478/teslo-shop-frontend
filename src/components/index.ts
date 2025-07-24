ebf8d2cb8204c2cbafc3e864c748dd504e14cb6f
Nelson Cadenas <nca1478@yahoo.com>
2025-07-24 02:45:44 -0400

    199. Menú lateral - Primera parte

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@
 export * from "./ui/title/Title";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/top-menu/TopMenu";
+export * from "./ui/sidebar/Sidebar";
 
 // products
 export * from "./products/product-grid/ProductGrid";
