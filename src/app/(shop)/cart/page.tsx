833618bee405ee3ca73f94315624ea79ed0fbdaa
Nelson Cadenas <nca1478@yahoo.com>
2025-07-25 15:42:46 -0400

    215. Pantalla carrito de compras

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const productsInCart = [initialData.products[0], initialData.products[1], initialData.products[2]];
+
 export default function CartPage() {
     return (
-        <div>
-            <h1>Cart Page</h1>
+        <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+            <div className="flex flex-col w-[1000px]">
+                <Title title="Carrito" />
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+                    {/* Carrito */}
+                    <div className="flex flex-col mt-5">
+                        <span className="text-xl">Agregar más items</span>
+                        <Link href="/" className="underline mb-5">
+                            Continúa comprando
+                        </Link>
+
+                        {/* Items */}
+                        {productsInCart.map((product) => (
+                            <div key={product.slug} className="flex mb-5">
+                                <Image
+                                    src={`/products/${product.images[0]}`}
+                                    width={100}
+                                    height={100}
+                                    style={{
+                                        width: "100px",
+                                        height: "100px",
+                                    }}
+                                    alt={product.title}
+                                    className="mr-5 rounded"
+                                />
+
+                                <div>
+                                    <p>{product.title}</p>
+                                    <p>${product.price}</p>
+                                    <QuantitySelector quantity={3} />
+
+                                    <button className="underline mt-3">Remover</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
