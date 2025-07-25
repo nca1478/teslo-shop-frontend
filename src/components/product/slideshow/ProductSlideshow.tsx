afbf75c942a3ede123243699b7147a13452a9b2c
Nelson Cadenas <nca1478@yahoo.com>
2025-07-25 13:13:50 -0400

    213. Slideshow con Thumbs - Parte 2

--- src/components/product/slideshow/ProductSlideshow.tsx
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import { useState } from "react";
+import Image from "next/image";
+import { Swiper as SwiperObject } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, FreeMode, Navigation, Thumbs } from "swiper/modules";
+
+// Import Swiper styles
 import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import "./slideshow.css";
 
 interface Props {
     images: string[];
@@ -10,18 +20,60 @@ interface Props {
 }
 
 export const ProductSlideshow = ({ images, title, className }: Props) => {
+    const [thumbsSwiper, setThumbsSwiper] = useState<SwiperObject>();
+
     return (
         <div className={className}>
             <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log("slide change")}
-                onSwiper={(swiper) => console.log(swiper)}
+                style={
+                    {
+                        "--swiper-navigation-color": "#fff",
+                        "--swiper-pagination-color": "#fff",
+                    } as React.CSSProperties
+                }
+                spaceBetween={10}
+                navigation={true}
+                autoplay={{
+                    delay: 2500,
+                }}
+                thumbs={{
+                    swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+                }}
+                modules={[FreeMode, Navigation, Thumbs, Autoplay]}
+                className="mySwiper2"
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <Image
+                            src={`/products/${image}`}
+                            alt={title}
+                            width={1024}
+                            height={800}
+                            className="rounded-l-lg object-fill"
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            <Swiper
+                onSwiper={setThumbsSwiper}
+                spaceBetween={10}
+                slidesPerView={4}
+                freeMode={true}
+                watchSlidesProgress={true}
+                modules={[FreeMode, Navigation, Thumbs]}
+                className="mySwiper"
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <Image
+                            src={`/products/${image}`}
+                            alt={title}
+                            width={300}
+                            height={300}
+                            className="rounded-l-lg object-fill"
+                        />
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
     );
