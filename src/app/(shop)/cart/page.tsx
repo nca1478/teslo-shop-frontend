90114c0e6d422b3133d10b25e6a95d6d9d290bfb
Nelson Cadenas <nca1478@yahoo.com>
2025-07-25 16:02:33 -0400

    216. Pantalla carrito de compras - Parte 2

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import Image from "next/image";
 import { QuantitySelector, Title } from "@/components";
 import { initialData } from "@/seed/seed";
+import Image from "next/image";
 
 const productsInCart = [initialData.products[0], initialData.products[1], initialData.products[2]];
 
@@ -43,6 +43,34 @@ export default function CartPage() {
                                 </div>
                             </div>
                         ))}
+                    </div>
+
+                    {/* Checkout - Resumen de orden */}
+                    <div className="bg-white rounded-xl shadow-xl p-7 h-fit">
+                        <h2 className="text-2xl mb-2">Resumen de orden</h2>
+
+                        <div className="grid grid-cols-2">
+                            <span>No. Productos</span>
+                            <span className="text-right">3 artículos</span>
+
+                            <span>Subtotal</span>
+                            <span className="text-right">$ 100</span>
+
+                            <span>Impuestos (15%)</span>
+                            <span className="text-right">$ 100</span>
+
+                            <span className="mt-5 text-2xl">Total:</span>
+                            <span className="mt-5 text-2xl text-right">$ 100</span>
+                        </div>
+
+                        <div className="mt-5 mb-2 w-full">
+                            <Link
+                                className="flex btn-primary justify-center"
+                                href="/checkout/address"
+                            >
+                                Checkout
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
