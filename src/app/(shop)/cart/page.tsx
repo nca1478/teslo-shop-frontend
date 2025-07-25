26819b8a9d1437bff5a713914194b33ad48ad8f4
Nelson Cadenas <nca1478@yahoo.com>
2025-07-25 17:16:06 -0400

    221. Pantalla de carrito de compras vacío

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 import { QuantitySelector, Title } from "@/components";
 import { initialData } from "@/seed/seed";
-import Image from "next/image";
 
 const productsInCart = [initialData.products[0], initialData.products[1], initialData.products[2]];
 
 export default function CartPage() {
+    // redirect("/empty");
+
     return (
         <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
             <div className="flex flex-col w-[1000px]">
