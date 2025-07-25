5f012445bff98f678646b4b47f4dcecbb89d39b0
Nelson Cadenas <nca1478@yahoo.com>
2025-07-25 00:22:18 -0400

    208. Pantalla de producto

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,45 @@
-export default function ProductPage() {
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+interface Props {
+    params: Promise<{ slug: string }>;
+}
+
+export default async function ProductPage({ params }: Props) {
+    const { slug } = await params;
+    const product = initialData.products.find((product) => product.slug === slug);
+
+    if (!product) {
+        notFound();
+    }
+
     return (
-        <div>
-            <h1>Product Page</h1>
+        <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+            {/* Slideshow */}
+            <div className="col-span-1 md:col-span-2">Hola</div>
+
+            {/* Detalles */}
+            <div className="col-span-1 px-5">
+                {/* Titulo */}
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+                    {product.title}
+                </h1>
+
+                {/* Precio */}
+                <p className="text-lg mb-5">${product.price}</p>
+
+                {/* Selector de Tallas */}
+
+                {/* Selector de Cantidad */}
+
+                {/* Button */}
+                <button className="btn-primary my-5">Agregar al carrito</button>
+
+                {/* Descripción */}
+                <h3 className="font-bold text-sm">Descripción</h3>
+                <p className="font-light">{product.description}</p>
+            </div>
         </div>
     );
 }
