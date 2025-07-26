30b7f3e87dff1320d9c1f5ffd37eefc78892df9e
Nelson Cadenas <nca1478@yahoo.com>
2025-07-26 19:08:36 -0400

    251. Paginación - Componente HTML

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,5 @@
 import { getPaginatedProductsWithImages } from "@/actions";
-import { ProductGrid, Title } from "@/components";
+import { Pagination, ProductGrid, Title } from "@/components";
 import { redirect } from "next/navigation";
 // import { initialData } from "@/seed/seed";
 
@@ -26,6 +26,8 @@ export default async function HomePage({ searchParams }: Props) {
             <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
 
             <ProductGrid products={products} />
+
+            <Pagination totalPages={totalPages} />
         </>
     );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export * from "./ui/not-found/PageNotFound";
 export * from "./ui/top-menu/TopMenu";
 export * from "./ui/sidebar/Sidebar";
 export * from "./ui/footer/Footer";
+export * from "./ui/pagination/Pagination";
 
 // products
 export * from "./products/product-grid/ProductGrid";
