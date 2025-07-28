70397e51155b524a68c0955f3e461ab8820cee59
Nelson Cadenas <nca1478@yahoo.com>
2025-07-28 16:48:28 -0400

    267. Skeleton - Mostrar loading en el stock

--- src/components/product/stock-label/StockLabel.tsx
+++ src/components/product/stock-label/StockLabel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getStockBySlug } from "@/actions";
 import { titleFont } from "@/config/fonts";
 
@@ -10,19 +10,32 @@ interface Props {
 
 export const StockLabel = ({ slug }: Props) => {
     const [stock, setStock] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
-    const getStock = async () => {
+    const getStock = useCallback(async () => {
         const inStock = await getStockBySlug(slug);
         setStock(inStock);
-    };
+        setIsLoading(false);
+    }, [slug]);
 
     useEffect(() => {
         getStock();
     }, [getStock]);
 
     return (
-        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
-            En Stock: {stock} Und.
-        </h1>
+        <>
+            {/* isLoading true : muestra el skeleton, false: muestra el stock */}
+            {isLoading ? (
+                <h1
+                    className={`${titleFont.className} antialiased font-bold bg-gray-200 animate-pulse text-xl mb-5`}
+                >
+                    &nbsp;
+                </h1>
+            ) : (
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+                    En Stock: {stock} Und.
+                </h1>
+            )}
+        </>
     );
 };
