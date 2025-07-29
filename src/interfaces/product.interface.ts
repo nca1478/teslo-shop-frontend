aa413638582ea62fe8a9a487f904141dfd8d3641
Nelson Cadenas <nca1478@yahoo.com>
2025-07-28 21:14:10 -0400

    278. Client Side - Cart Store

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -12,6 +12,16 @@ export interface Product {
     gender: Gender;
 }
 
+export interface CartProduct {
+    id: string;
+    slug: string;
+    title: string;
+    price: number;
+    quantity: number;
+    size: Size;
+    image: string;
+}
+
 export type Gender = "men" | "women" | "kids" | "unisex";
 export type Size = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
 export type Type = "shirts" | "pants" | "hoodies" | "hats";
---
+++ src/store/cart/cart-store.ts
@@ -0,0 +1,13 @@
+import { CartProduct } from "@/interfaces";
+import { create } from "zustand";
+
+interface State {
+    cart: CartProduct[];
+    //   addProductToCart:
+    //   updateProductQuantity:
+    //   removeProduct:
+}
+
+export const useCartStore = create<State>()((set) => ({
+    cart: [],
+}));
