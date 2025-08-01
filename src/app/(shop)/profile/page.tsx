bab52732957a8b4b46b5c941b634ba0db4af40ff
Nelson Cadenas <nca1478@yahoo.com>
2025-08-01 15:43:49 -0400

    306. Expandir el objeto de sesión

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -1,5 +1,5 @@
-import { auth } from "@/auth.config";
 import { Title } from "@/components";
+import { auth } from "@/config";
 import { redirect } from "next/navigation";
 
 export default async function ProfilePage() {
@@ -14,6 +14,8 @@ export default async function ProfilePage() {
             <Title title="Perfil" />
 
             <pre className="mb-4 p-4">{JSON.stringify(session?.user, null, 2)}</pre>
+
+            <h3 className="text-3xl mb-10">{session.user.role}</h3>
         </div>
     );
 }
--- src/config/auth/auth.config.ts
+++ src/config/auth/auth.config.ts
@@ -9,6 +9,24 @@ export const authConfig: NextAuthConfig = {
         signIn: "/auth/login",
         newUser: "/auth/new-account",
     },
+
+    callbacks: {
+        jwt({ token, user }) {
+            if (user) {
+                token.data = user;
+            }
+
+            return token;
+        },
+
+        session({ session, token }) {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            session.user = token.data as any;
+
+            return session;
+        },
+    },
+
     providers: [
         Credentials({
             async authorize(credentials) {
