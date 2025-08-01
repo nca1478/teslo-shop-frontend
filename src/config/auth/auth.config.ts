fd7efcc781663a478739f53a43c0d2cb9dc2d377
Nelson Cadenas <nca1478@yahoo.com>
2025-08-01 19:13:56 -0400

    312. Middleware

--- src/config/auth/auth.config.ts
+++ src/config/auth/auth.config.ts
@@ -25,6 +25,17 @@ export const authConfig: NextAuthConfig = {
 
             return session;
         },
+
+        // authorized({ auth, request: { nextUrl } }) {
+        //   const isLoggedIn = !!auth?.user;
+        //   const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+        //   if (isOnDashboard) {
+        //     if (isLoggedIn) return true;
+        //     return false; // Redirect unauthenticated users to login page
+        //   } else if (isLoggedIn) {
+        //     return Response.redirect(new URL("/dashboard", nextUrl));
+        //   }
+        // },
     },
 
     providers: [
