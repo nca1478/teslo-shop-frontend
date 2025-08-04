12c6e02c8a99bf1183e4d85c45b481c855482ff8
Nelson Cadenas <nca1478@yahoo.com>
2025-08-04 19:48:57 -0400

    322. Mostrar los países en el formulario

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,6 +3,9 @@ export * from "./auth/login";
 export * from "./auth/logout";
 export * from "./auth/register";
 
+// country
+export * from "./country/get-countries";
+
 // product
 export * from "./product/get-product-by-slug";
 export * from "./product/product-pagination";
