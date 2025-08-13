0be636808bb592d5382f167c5ae278e2a46af6ce
Nelson Cadenas <nca1478@yahoo.com>
2025-08-12 20:07:55 -0400

    344. Colocar orden - Total, SubTotal e Impuesto

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -31,6 +31,29 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
         },
     });
 
-    // Calcular los montos
+    // Calcular items de la orden
     const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0);
+
+    // Calcular subtotal, tax y total
+    const { subTotal, tax, total } = productIds.reduce(
+        (totals, item) => {
+            const productQuantity = item.quantity;
+            const product = products.find((product) => product.id === item.productId);
+
+            if (!product) throw new Error(`${item.productId} no existe`);
+
+            const subTotal = product.price * productQuantity;
+
+            totals.subTotal += subTotal;
+            totals.tax += subTotal * 0.15;
+            totals.total += subTotal * 1.15;
+
+            return totals;
+        },
+        { subTotal: 0, tax: 0, total: 0 }
+    );
+
+    console.log({ itemsInOrder, subTotal, tax, total });
+
+    // crear la transacción de la bd
 };
