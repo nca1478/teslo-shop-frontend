faa01206bed54c887bff1b1e35a7cb70a8a2fed3
Nelson Cadenas <nca1478@yahoo.com>
2025-08-15 14:02:54 -0400

    387. Actualizar producto

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -1,13 +1,20 @@
 "use server";
 
-import { Gender } from "@/generated/prisma";
 import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+import { Gender, Product } from "@/generated/prisma";
+import { Size } from "@/interfaces";
 
 // Validación de datos del formulario
 const productSchema = z.object({
     id: z.uuid().optional().nullable(),
     title: z.string().min(3).max(255),
-    slug: z.string().min(3).max(255),
+    // slug: z.string().min(3).max(255),
+    slug: z.coerce
+        .string()
+        .min(3)
+        .max(255)
+        .transform((val) => val.toLowerCase().replace(/ /g, "_").trim()),
     description: z.string(),
     price: z.coerce
         .number()
@@ -32,9 +39,51 @@ export const createUpdateProduct = async (formData: FormData) => {
         return { ok: false };
     }
 
-    console.log(productParsed.data);
+    const product = productParsed.data;
 
-    return {
-        ok: true,
-    };
+    // transforma slug a url amigable (alternativa a zod)
+    // product.slug = product.slug.toLowerCase().replace(/ /g, "_").trim();
+
+    const { id, ...rest } = product;
+
+    try {
+        const prismaTx = await prisma.$transaction(async (tx) => {
+            let product;
+            const tagsArray = rest.tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+            if (id) {
+                // Actualizar
+                product = await tx.product.update({
+                    where: { id },
+                    data: {
+                        ...rest,
+                        sizes: {
+                            set: rest.sizes as Size[],
+                        },
+                        tags: {
+                            set: tagsArray,
+                        },
+                    },
+                });
+
+                console.log({ updatedProduct: product });
+            } else {
+                // Crear
+            }
+
+            return {
+                ok: true,
+            };
+        });
+
+        return {
+            ok: true,
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            ok: false,
+            message: "Revisar los logs, no se pudo actualizar/crear",
+        };
+    }
 };
