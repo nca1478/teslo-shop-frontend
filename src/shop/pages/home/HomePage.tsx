78b66690daff12cde299dfdd8725b436fd606b56
Nelson Cadenas <nca1478@yahoo.com>
2025-10-13 15:01:56 -0400

    263. HomePage - Paginación y productos

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,16 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/products.mock";
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
 
 export const HomePage = () => {
     return (
         <div>
             <CustomJumbotron title="Todos los Productos" />
+
+            <ProductsGrid products={products} />
+
+            <CustomPagination totalPages={7} />
         </div>
     );
 };
