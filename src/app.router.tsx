de6bb8f29cc2ecb9588d3ff5b16951493056456d
Nelson Cadenas <nca1478@yahoo.com>
2025-10-13 14:14:03 -0400

    261. Tailwind + ShadCN UI + Google Fonts

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,14 +1,14 @@
 import { lazy } from "react";
 import { createBrowserRouter, Navigate } from "react-router";
-import { ShopLayout } from "./shop/layouts/ShopLayout";
-import { HomePage } from "./shop/pages/home/HomePage";
-import { ProductPage } from "./shop/pages/product/ProductPage";
-import { GenderPage } from "./shop/pages/gender/GenderPage";
 import { DashboardPage } from "./admin/pages/dashboard/DashboardPage";
 import { AdminProductPage } from "./admin/pages/product/AdminProductPage";
 import { AdminProductsPage } from "./admin/pages/products/AdminProductsPage";
 import { LoginPage } from "./auth/pages/login/LoginPage";
 import { RegisterPage } from "./auth/pages/register/RegisterPage";
+import { ShopLayout } from "./shop/layouts/ShopLayout";
+import { GenderPage } from "./shop/pages/gender/GenderPage";
+import { HomePage } from "./shop/pages/home/HomePage";
+import { ProductPage } from "./shop/pages/product/ProductPage";
 
 const AuthLayout = lazy(() => import("./auth/layouts/AuthLayout"));
 const AdminLayout = lazy(() => import("./admin/layouts/AdminLayout"));
--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+    return (
+        <div>
+            <h1>HomePage</h1>
+            <h1 className="font-montserrat">HomePage</h1>
+            <h1 className="font-montserrat font-thin">HomePage</h1>
+            <h1 className="font-montserrat font-normal">HomePage</h1>
+            <h1 className="font-montserrat font-bold">HomePage</h1>
+
+            <Button variant="default">Enviar</Button>
+        </div>
+    );
 };
