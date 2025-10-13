2ce9b0abcbccea80adc8b5dd5c4a1e78675b6d39
Nelson Cadenas <nca1478@yahoo.com>
2025-10-13 14:38:42 -0400

    262. Diseño del HomePage

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,15 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
 
 export const HomePage = () => {
     return (
         <div>
-            <h1>HomePage</h1>
-            <h1 className="font-montserrat">HomePage</h1>
-            <h1 className="font-montserrat font-thin">HomePage</h1>
-            <h1 className="font-montserrat font-normal">HomePage</h1>
-            <h1 className="font-montserrat font-bold">HomePage</h1>
-
-            <Button variant="default">Enviar</Button>
+            <CustomJumbotron title="Todos los Productos" />
         </div>
     );
 };
