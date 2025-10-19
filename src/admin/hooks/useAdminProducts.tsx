33b43f76a2fc2f12edc3f4bd7609898762e6221d
Nelson Cadenas <nca1478@yahoo.com>
2025-10-19 01:22:32 -0400

    999. Ajuste en useAdminProduct (agregado parametro query para busqueda de productos)

--- src/admin/hooks/useAdminProducts.tsx
+++ src/admin/hooks/useAdminProducts.tsx
@@ -1,10 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
 import { getProductsAction } from "@/shop/actions/get-products.action";
 
 export const useAdminProducts = () => {
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get("query") || "";
+
     return useQuery({
-        queryKey: ["products", { limit: 50, offset: 0 }], // Parámetros fijos para admin
-        queryFn: () => getProductsAction({ limit: 50, offset: 0 }),
+        queryKey: ["products", { limit: 50, offset: 0, query }],
+        queryFn: () => getProductsAction({ limit: 50, offset: 0, query }),
         staleTime: 0, // Siempre considerar los datos como stale en admin
         refetchOnWindowFocus: true,
         refetchOnMount: true,
