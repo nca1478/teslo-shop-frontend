9f74b53a478013110e73dfd10c7414fa3cd32161
Nelson Cadenas <nca1478@yahoo.com>
2025-12-17 21:06:34 -0400

    fixed: prisma logs settings

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,14 @@
 //  Evitando múltiples instancias de Prisma Client
 //  https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help
 
+import dotenv from "dotenv";
 import { PrismaClient } from "../generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const connectionString =
-    process.env.DATABASE_URL ||
-    "postgresql://postgres:postgres@localhost:5433/teslo-shop?schema=public";
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
@@ -18,7 +19,7 @@ export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
         adapter,
-        log: ["query"],
+        log: process.env.NODE_ENV === "production" ? ["error"] : [],
     });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
