23e78cb222bd8e7aa715d82689cb14629c858c46
Nelson Cadenas <nca1478@yahoo.com>
2025-12-19 14:28:56 -0400

    999. Arreglado error al presionar crear producto (admin)

--- src/app/(shop)/admin/product/[slug]/page.tsx
+++ src/app/(shop)/admin/product/[slug]/page.tsx
@@ -9,7 +9,12 @@ interface Props {
 
 export default async function ProductPage({ params }: Props) {
     const { slug } = await params;
-    const [product, categories] = await Promise.all([getProductBySlug(slug), getCategories()]);
+
+    // Si es un producto nuevo, no intentar buscar el producto
+    const [product, categories] = await Promise.all([
+        slug === "new" ? Promise.resolve(null) : getProductBySlug(slug),
+        getCategories(),
+    ]);
 
     if (!product && slug !== "new") {
         redirect("/admin/products");
